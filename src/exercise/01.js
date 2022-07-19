aa7280c149056bc5fcac5e4311cab006c3ccc472
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-19 22:37:34 +0530

    01 extra 3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,17 +4,21 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  const countReducer = (previousCount, newState) => ({
-    ...previousCount,
-    count: newState.count,
-  })
+  const countReducer = (previousState, newState) => {
+    if (typeof newState === 'function') return newState(previousState)
+    return {
+      ...previousState,
+      count: newState.count,
+    }
+  }
 
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
 
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
